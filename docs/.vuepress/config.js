93b39e3f3b7dbdc3a4f0c579b203142b03e70592
LingBlopdreame <782954119@qq.com>
2021-02-25 23:40:00 +0800

    add

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,90 @@
+module.exports = {
+	base: '/site/',
+	title: 'LingBlopdreame',
+	description: 'LingBlopdreame De Playgroud, Enjoy Your Life',
+	author: 'LingBlopdreame',
+	head: [
+		['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+		['link', { rel: 'icon', href: '/assets/img/logo.png'}]
+	],
+	theme: 'reco',
+	themeConfig: {
+		type: 'blog',
+		authorAvatar: '/assets/img/logo.png',
+		noFoundPageByTencent: false,
+		subSidebar: 'auto',
+		blogConfig: {
+			category: {
+			  	location: 2,     // 在导航栏菜单中所占的位置，默认2
+			  	text: 'Category' // 默认文案 “分类”
+			},
+			tag: {
+			  	location: 3,     // 在导航栏菜单中所占的位置，默认3
+			  	text: 'Tag'      // 默认文案 “标签”
+			},
+			socialLinks: [     // 信息栏展示社交信息
+			  	{ icon: 'reco-github', link: 'https://github.com/LingBlopdreame' },
+			  	{ icon: 'reco-mayun', link: 'https://gitee.com/LingBlopdreame' },
+				{ icon: 'reco-blog', link: '/' },
+				{ icon: 'reco-mail', text: '782954119@qq.com'}
+			]
+		},
+		nav: 
+		[
+			{
+				text: 'Home',
+				link: '/',
+				icon: 'reco-home',
+			},
+			{ 
+				text: 'TimeLine', 
+				link: '/timeline/', 
+				icon: 'reco-date' },
+			{
+				text: 'BiliBili',
+				link: 'https://www.bilibili.com',
+				icon: 'reco-bilibili'
+			}, 
+			{
+				text: 'Gitee',
+				link: 'https://www.gitee.com/LingBlopdreame',
+				icon: 'reco-mayun'
+			},
+			{
+				text: 'GitHub',
+				link: 'https://www.github.com/LingBlopdreame',
+				icon: 'reco-github'
+			}, 
+		],
+		friendLink: [
+			{
+				title: 'vuepress-theme-reco',
+				desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+				logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+				link: 'https://vuepress-theme-reco.recoluan.com'
+			},
+		]
+ 	},
+	plugins: [
+		[
+		  	'vuepress-plugin-mathjax',
+		  	{
+				target: 'svg',
+				macros: {
+			  		'*': '\\times',
+				},
+		  	},
+		],
+		[ 'flowchart' ],
+		[
+			"@vuepress-reco/vuepress-plugin-kan-ban-niang",
+			{
+				theme: ['haru1', 'haru2', 'koharu', 'shizuku', 'z16'],
+				clean: true,
+			}
+		],
+		[
+			'@vuepress-reco/vuepress-plugin-pagation',
+		]
+	],
+}
---
+++ docs/README.md
@@ -0,0 +1,19 @@
+---
+home: true
+# heroImage: /assets/img/hero.jpg
+# heroImageStyle: {
+#   maxHeight: '200px',
+#   display: block,
+#   margin: '6rem auto 1.5rem',
+# #   borderRadius: '50%',
+#   boxShadow: '0 5px 18px rgba(0,0,0,0.2)'
+# }
+# heroText: null
+bgImage: '/assets/img/bg1.jpg'
+bgImageStyle: {
+  height: '500px'
+}
+heroText: Just Start Your Enjoy
+tagline: Do It ! ! !
+---
+
